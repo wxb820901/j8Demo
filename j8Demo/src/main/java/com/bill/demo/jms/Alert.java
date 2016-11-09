e4bec3a4995c09e7807cd5ba7649be721d0e782b
wxb820901 <wxb820901@gmail.com>
2016-11-09 17:37:02 +0800

    Create Alert.java

---
+++ j8Demo/src/main/java/com/bill/demo/jms/Alert.java
@@ -0,0 +1,28 @@
+
+
+import org.springframework.jms.core.MessageCreator;
+
+import javax.jms.*;
+
+
+
+public class Alert implements MessageCreator {
+    public NeoAlert(){
+
+    }
+
+    private String uid;
+    
+    @Override
+    public Message createMessage(Session session) throws JMSException {
+        return createMapMessage(session);
+    }
+
+    public MapMessage createMapMessage(Session session) throws JMSException {
+        MapMessage message = session.createMapMessage();
+        message.setJMSType("ALERTS_NOTIFICATION");//header type
+        message.setString("id", id);
+       
+        return message;
+    }
+}
