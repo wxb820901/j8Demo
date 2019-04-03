679b1988bca07bcb0d4bc1996073bc52a3db8c13
wxb820901@gmail.com <wxb820901@gmail.com>
2019-04-03 14:28:20 +0800

    20190403_add_case_4_jconsole

---
+++ src/test/java/com/bill/demo/j8/JConsoleTest.java
@@ -0,0 +1,30 @@
+package com.bill.demo.j8;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//start current case and tehn run jconsole
+public class JConsoleTest {
+
+    static class OOMObject {
+        public byte[] placeholder = new byte[64 * 1024];
+    }
+
+    public static void fillHeap(int num) throws InterruptedException {
+        Thread.sleep(20000);
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        for (int i = 0; i < num; i++) {
+            Thread.sleep(50);
+            list.add(new OOMObject());
+        }
+        System.gc();
+    }
+
+    @Test
+    public void testJConsole() throws InterruptedException {
+        fillHeap(1000);
+        while(true){}
+    }
+}
